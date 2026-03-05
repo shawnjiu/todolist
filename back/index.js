0c517b45decd308e8fea3616ffb0506cda5c69de
taotanqiu <2675204883@qq.com>
2026-03-04 18:45:22 -0700

    todolist

--- back/index.js
+++ back/index.js
@@ -33,6 +33,10 @@ connectDB();
 
 app.use("/api/todos", todoRoutes)
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}.....`)
