4e522d2473c582c6eaaf3351466896a25aa7022d
taotanqiu <2675204883@qq.com>
2026-03-04 20:39:58 -0700

    todolist

--- back/index.js
+++ back/index.js
@@ -33,8 +33,8 @@ connectDB();
 
 app.use("/api/todos", todoRoutes)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use((req, res) => {
+  res.status(404).send('页面未找到');
 });
 
 const PORT = process.env.PORT || 5000
