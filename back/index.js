ab56c6313d0ae48cd72ca2de09314a46ac4901a0
taotanqiu <2675204883@qq.com>
2026-03-04 21:49:29 -0700

    fix

--- back/index.js
+++ back/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(distPath));
 
-  app.get('/:any*', (req, res) => {
+  app.get(/.*/, (req, res) => {
  
     res.sendFile(path.join(distPath, 'index.html'));
   });
